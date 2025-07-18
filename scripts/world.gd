a00e9b658940f157e0603e5b38a413b1453114ef
My-game-journal <138797981+My-game-journal@users.noreply.github.com>
2025-07-18 22:59:19 +0200

    Delete scripts directory

--- scripts/world.gd
+++
@@ -1,7 +0,0 @@
-# World.gd
-extends Node
-
-func _input(event):
-	if event.is_action_pressed("pause_menu_button"):
-		$CanvasLayer/PausedMenu.visible = true
-		get_tree().paused = true  # Fixed typo here
