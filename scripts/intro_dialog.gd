d852c918d95d1775fbf8af3a729d0768d10e24b1
My-game-journal <138797981+My-game-journal@users.noreply.github.com>
2025-07-20 23:15:42 +0200

    Update intro_dialog.gd

--- scripts/intro_dialog.gd
+++ scripts/intro_dialog.gd
@@ -3,7 +3,7 @@ extends RichTextLabel
 @onready var label = $/root/world/level_layers/IntroDialog/RichTextLabel
 
 func _ready():
-	label.visible = true  # Upewniamy się, że label się pokaże
+	label.visible = true  
 	await get_tree().create_timer(20.0).timeout
-	label.visible = false  # Ukrycie po 5 sekundach
+	label.visible = false  
 	
